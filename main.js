ec97608e81a3c89753f915dbc135b3b828bf4cfa
Lauprof <92731572+Lauprof@users.noreply.github.com>
2023-04-14 08:48:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+
+//let classname = document.body.childNodes[1];
+//alert(classname);
+//let img = classname.getElementById('img1');
+//img.src ='image5.jpg';
+
+
+//document.getElementById('script').innerHTML = classname;
